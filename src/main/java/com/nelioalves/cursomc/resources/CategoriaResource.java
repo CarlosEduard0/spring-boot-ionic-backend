fee7ae18b16c37e78d07cdd3cb7907173ad29f4b
Carlos Eduardo <carloseduardofox8@gmail.com>
2019-01-24 09:13:31 -0300

    Criando repository e service para Categoria

--- src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
@@ -1,26 +1,26 @@
 package com.nelioalves.cursomc.resources;
 
-import com.nelioalves.cursomc.domain.Categoria;
+import com.nelioalves.cursomc.services.CategoriaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @RestController
 @RequestMapping("/categorias")
 public class CategoriaResource {
 
-    @GetMapping
-    public List<Categoria> listar() {
-        Categoria cat1 = new Categoria(1L, "Informática");
-        Categoria cat2 = new Categoria(2L, "Escritório");
+    private final CategoriaService categoriaService;
 
-        List<Categoria> lista = new ArrayList<>();
-        lista.add(cat1);
-        lista.add(cat2);
+    @Autowired
+    public CategoriaResource(CategoriaService categoriaService) {
+        this.categoriaService = categoriaService;
+    }
 
-        return lista;
+    @GetMapping(path = "{id}")
+    public ResponseEntity<?> find(@PathVariable Long id) {
+        return ResponseEntity.ok().body(categoriaService.buscar(id));
     }
 }
---
+++ src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
@@ -0,0 +1,24 @@
+package com.nelioalves.cursomc.services;
+
+import com.nelioalves.cursomc.domain.Categoria;
+import com.nelioalves.cursomc.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CategoriaService {
+
+    private final CategoriaRepository categoriaRepository;
+
+    @Autowired
+    public CategoriaService(CategoriaRepository categoriaRepository) {
+        this.categoriaRepository = categoriaRepository;
+    }
+
+    public Categoria buscar(Long id) {
+        Optional<Categoria> categoria = categoriaRepository.findById(id);
+        return categoria.orElse(null);
+    }
+}
